ea716177403355d494f05ca8b51df46f1bcef01e
unknown <ballmarsin2002@gmail.com>
2018-06-16 00:51:24 +0700

    fix navbar

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => res.render('about'));
 
-app.get('/admin', (req, res) => res.send('https://itunes.apple.com/ca/app/dofus-touch/id1041406978'));
-
 app.get('*', (req, res) => res.status(404).send("404 Error - Page not found"));
 
 const PORT = process.env.PORT || 3000;
